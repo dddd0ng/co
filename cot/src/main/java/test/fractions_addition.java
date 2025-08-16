6ec749ad70ea455f243b85fbfbee3551cdbb439e
dddd0ng <kdg1644@naver.com>
2025-08-16 17:26:15 +0900

    o

--- cot/src/main/java/test/fractions_addition.java
+++ cot/src/main/java/test/fractions_addition.java
@@ -2,6 +2,8 @@ package test;
 
 public class fractions_addition {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+       // numer1 첫번째 분자, denom1 첫번째 분모
+        // numer2 두번째 분자, denom2 두번째 분모
         int denom = denom1 * denom2;
         int numer = (numer1 * denom2) + (numer2 * denom1);
         int max = 0;
