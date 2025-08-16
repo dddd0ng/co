a09e674d4d2a9c9f51ec941b19f0c51860e6c64d
dddd0ng <kdg1644@naver.com>
2025-08-16 17:15:38 +0900

    t

--- cot/src/main/java/test/fractions_addition.java
+++ cot/src/main/java/test/fractions_addition.java
@@ -2,6 +2,9 @@ package test;
 
 public class fractions_addition {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int denom=denom1*denom2;
+        int numer=(numer1*denom2)+(numer2*denom1);
+
         int[] answer = {};
         return answer;
     }
