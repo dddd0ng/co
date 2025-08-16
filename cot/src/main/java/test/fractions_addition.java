1cfc2db8517f8592cc4d52186dfbdc18573b7d61
dddd0ng <kdg1644@naver.com>
2025-08-16 17:10:55 +0900

    q

--- cot/src/main/java/test/fractions_addition.java
+++ cot/src/main/java/test/fractions_addition.java
@@ -1,7 +1,8 @@
 package test;
 
 public class fractions_addition {
-    public static void main(String[] args) {
-
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = {};
+        return answer;
     }
 }
