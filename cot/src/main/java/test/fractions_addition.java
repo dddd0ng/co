643b26eaa0f888c486fa6fd9f542a7bdef9ab5ea
dddd0ng <kdg1644@naver.com>
2025-08-16 17:18:01 +0900

    t

--- cot/src/main/java/test/fractions_addition.java
+++ cot/src/main/java/test/fractions_addition.java
@@ -2,10 +2,16 @@ package test;
 
 public class fractions_addition {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int denom=denom1*denom2;
-        int numer=(numer1*denom2)+(numer2*denom1);
+        int denom = denom1 * denom2;
+        int numer = (numer1 * denom2) + (numer2 * denom1);
+        int max = 0;
+        for (int i = 1; i <= numer && i <= denom; i++) {
+            if (numer % i == 0 && denom % i == 0) max = i;
+        }
+        numer=numer/max;
+        denom=denom/max;
 
-        int[] answer = {};
+        int[] answer = {numer,denom};
         return answer;
     }
 }
